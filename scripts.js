8c0ce28a6a5d9f57c61e9e0bbc0660cacf66c108
Yazzie01 <122305485+Yazzie01@users.noreply.github.com>
2024-04-18 13:57:22 -0700

    Added Javascript and edited CSS
    Working on Javascript functionality and making adjustments to CSS styles in order to correlate design.

---
+++ scripts.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/* Dark Mode*/
+var icon = document.getElementById("dark")
+dark.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.256x256.png";
+    }else{
+        icon.src = "images/moon.256x256.png";
+    }
+}
+/* Display section buttons*/
+
+
+/*Guessing Game*/
+
+let button = document.getElementById("gamePlay"); /*Calls the button from the HTML page */
+
+function guessingGame(){
+    /*Variables connecting to the HTML elements through id selection*/
+    let inputDisplay = document.getElementById("userGuess");
+    let gameMessage = document.getElementById("gameMsg");
+    let randNumFrom0To1 = Math.random()
+    let randNumberToTen = Math.floor(randNumFrom0To1 *10)+1;
+    let userGuess = inputDisplay.value;
+
+    /*Game function layout. Starting form user guess and calculated between 1 - 10*/
+    if(parseInt(userGuess) > 10 || parseInt(userGuess) < 1){
+        alert("Please enter a number between 1 and 10"); 
+    }else{
+        if (randNumberToTen === parseInt(userGuess)){
+            gameMessage.innerHTML = `Congrats you got it right! Your answer was ${userGuess} and the correct answer was ${randNumberToTen}`
+        }else{
+            gameMessage.innerHTML = `Sorry, about that! You guessed ${userGuess} and the correct answer was ${randNumberToTen}`
+        } 
+}
+/*Displayed in the console in order to test responses*/
+    console.log(userGuess);
+    console.log(randNumberToTen);
+}
+/*Listens to the event of a click when value is entered into the input*/
+button.addEventListener("click", function(e){
+    e.preventDefault();
+    guessingGame();
+});
+
+
+
+/* Form Validation*/
+
+
+/*Thank you Message pop-up*/
