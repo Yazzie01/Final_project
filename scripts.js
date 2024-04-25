907a932544d7867e8c0dcc4acdb168b332949fca
Kiara Yazzie <122305485+Yazzie01@users.noreply.github.com>
2024-04-24 18:02:59 -0700

    HTML Validation | Javascript cleanup
    Continued validation and code/comment clean up

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* Dark Mode*/
-var icon = document.getElementById("dark")
+let icon = document.getElementById("dark")
 dark.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
@@ -117,15 +117,13 @@ activeImg.src = activePoster.imgsrc;
 let button = document.getElementById("gamePlay"); /*Calls the button from the HTML page */
 
 function guessingGame(){
-        
     /*Variables connecting to the HTML elements through id selection*/
     let inputDisplay = document.getElementById("userGuess");
     let gameMessage = document.getElementById("gameMsg");
     let randNumFrom0To1 = Math.random()
     let randNumberToTen = Math.floor(randNumFrom0To1 *10) + 1;
     let userGuess = inputDisplay.value;
-    
-    e.preventDefault();
+
     /*Game function layout. Starting form user guess and calculated between 1 - 10*/
     if(parseInt(userGuess) > 10 || parseInt(userGuess) < 1){
         alert("Please enter a number between 1 and 10"); 
@@ -146,12 +144,9 @@ function guessingGame(){
 }
 /*Listens to the event of a click when value is entered into the input*/
 button.addEventListener("click", function(e){
+    e.preventDefault();
     guessingGame();
 });
-
-
-
-
 
 /* Form Validation*/
 
